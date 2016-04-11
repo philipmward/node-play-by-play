1ab3fdaaf4551f3a845eb043c60d1d5de5e07731
philipmward <philipmward@hotmail.com>
2016-04-11 14:48:59 -0700

    async pet server

--- routes/pet.js
+++ routes/pet.js
@@ -1,17 +1,47 @@
-var r = require('request').defaults({ //request allows you to make request to other services
+var request = require('request').defaults({ //request allows you to make request to other services
     json: true
 });
+
+var async = require('async');
 
 module.exports = function(app){
 
     // Read
     app.get('/pets', function(req, res){
-        r({uri: 'http://localhost:3001/dog'}, function(error, response, body){
-            if(!error && response.statusCode === 200){
-                res.json(body);
-            } else {
-                res.send(response.statusCode);
-            }
-        })
-    })
-}
+
+        async.parallel({
+
+        dog: function(callback){
+            request({uri: 'http://localhost:3001/dog'}, function(error, response, body){
+                if(error){
+                    callback({service: 'dog', error:error});
+                    return;
+                }
+                if(!error && response.statusCode === 200){
+                    callback(null, body.data);
+                } else{
+                    callback(response.statusCode);
+                }
+            });
+        },
+        cat: function(callback) {
+            request({uri: 'http://localhost:3000/cat'}, function (error, response, body) {
+                if(error){
+                    callback({service: 'cat', error:error});
+                    return;
+                }
+                if(!error && response.statusCode === 200){
+                    callback(null, body.data);
+                } else{
+                    callback(response.statusCode);
+                }
+            });
+        }
+    }, function(error, results){
+            res.json({
+                error: error,
+                results: results
+            });
+        });
+    });
+};
